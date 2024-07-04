d1451a38382f3387648814d7416e95474f450f60
code-iddih <idinyamwaya@gmail.com>
2024-07-04 23:00:55 +0300

    Adding an Event Listener

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+
+     // Getting the Button element
+    const input = document.getElementById('button');
+
+    // Adding the Event Listener and passing 2 Arguments
+    input.addEventListener('click', clickAlert); 
 }
+
+// The funtion that  executes when the User Clicks
+function clickAlert() {
+  alert('I was clicked!');
+}
+
